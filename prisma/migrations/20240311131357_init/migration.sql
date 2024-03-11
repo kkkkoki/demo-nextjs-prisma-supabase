2a64a2718d1f6908bb7f0b4ca3c6713adf7f9470
kkkoki <kokiinoue45@gmail.com>
2024-03-11 22:14:31 +0900

    init

---
+++ prisma/migrations/20240311131357_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "names" TEXT NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
